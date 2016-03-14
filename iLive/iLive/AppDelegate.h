57bb09816149121c3c448b412e4b290440b390d7
tinreslutely <tinreslutely@hotmail.com>
2016-03-14 15:56:50 +0800

    initiation project

---
+++ iLive/iLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iLive
+//
+//  Created by 李晓毅 on 16/3/14.
+//  Copyright © 2016年 301 Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iLive/iLive/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iLive
+//
+//  Created by 李晓毅 on 16/3/14.
+//  Copyright © 2016年 301 Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
